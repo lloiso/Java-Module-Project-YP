9a85e3a95b790df816e2f3194108f20383b023fe
dmitrii <llosio@mail.ru>
2023-03-03 10:08:23 +0300

    init commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
+
 // dev branch for Y.Practicum
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        System.out.println("vvedi > 1");
+
+        int a = scanner.nextInt();
+
+        while (a<=1){
+            if (a<1){
+                System.out.println("ne na kogo delit");
+            } else {
+                System.out.println("sam plati");
+            }
+            a = scanner.nextInt();
+        }
+        System.out.println(a);
+
+
+        System.out.println("tovar?");
+        String s = scanner.next();
+        while (!s.equals("йцу")){
+            System.out.println("tovar?");
+            s = scanner.next();
+        }
+        System.out.println("year boi");
+
+
+
     }
+
+
 }
